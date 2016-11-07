a7849fa400adaf9f2efa089d72ac2529fc22a677
LeTrongTuan <1312660@student.hcmus.edu.vn>
2016-11-07 10:21:37 +0700

    Chức năng hiện thị report

---
+++ OceanBlack/OceanBlack/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OceanBlack.Models;
+
+namespace OceanBlack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DemoEntities db = new DemoEntities();
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.ListData = db.products.ToList();
+            return View();
+        }
+
+        public ActionResult LoadDataOnView(report model)
+        {
+            var date = model.date_transation;
+            return View(db.XuatReports(date));
+        }
+    }
+}
